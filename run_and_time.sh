1aac79e242336f0d9f6df9b4f9d2240652c19233
Ryan Newton <rrnewton@gmail.com>
2015-07-09 21:52:44 -0400

    Fix TEE call

--- run_and_time.sh
+++ run_and_time.sh
@@ -2,10 +2,12 @@
 
 set -xe
 
+export PATH=$HOME/racket-6.2.0.5/bin:$PATH
+
 # Reset Racket in a primitive way:
 rm -rf ~/racket-6.2.0.5
 cp -a ~/racket-6.2.0.5-bak ~/racket-6.2.0.5
 
 # Now run and get our timings.
 
-raco pkg update typed-racket-lib/ | tee timings.log
+(raco pkg update typed-racket-lib/ 2>&1) | tee timings.log
